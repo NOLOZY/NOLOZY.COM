09c627da0732294023886376767360f391859c4a
NOLOZY <65292927+NOLOZY@users.noreply.github.com>
2020-07-04 00:08:31 +0900

    1.0

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>NOLOZY.COM</title>
+<style>
+</style>
+</head>
+<body>
+    <div class="header">
+        <h1>NOLOZY.COMへようこそ！</h1>
+        <br><br>
+    </div>
+    <div class="content">
+        <a href="https://www.geartics.com/NOLOZY">Geartics</a>(デバイス)<br>
+        <iframe src="https://discordapp.com/widget?id=605747988047855626&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+        <a href="https://twitter.com/NOLOZY">Twitter</a><br>
+        <a href="https://www.youtube.com/channel/UCEQ8cNKq4Wlvl5P2FDaCM4g">YouTube</a><br>
+        <a href="https://www.twitch.tv/vt_nolozy">Twitch</a><br>
+        <a href="https://www.mildom.com/10425573">Mildom</a><br>
+        <br><br>
+        <h3>MY WORKS</h3><br>
+        <a href="#">Coming Soon...</a><br>
+        とあるメディアサイト<br>
+        <a href="hhttps://pozt.tokyo/">POZT.TOKYO</a><br>
+        Twitterのタイムラインを見ることなくツイートできるサイト。<br>
+    </div>
+    <div class="footer">
+        <br><br>
+        🄫 NOLOZY.COM
+    </div>
+</body>
+</html>
